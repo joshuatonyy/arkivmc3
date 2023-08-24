8e04079fff10b800cabf5abb75c60c2966819b98
Joshua Tony <64730665+joshuatonyy@users.noreply.github.com>
2023-08-24 11:56:13 +0700

    Initial Commit

---
+++ arkivmc3/arkivmc3App.swift
@@ -0,0 +1,17 @@
+//
+//  arkivmc3App.swift
+//  arkivmc3
+//
+//  Created by Joshua on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct arkivmc3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
